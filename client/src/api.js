ac160e86f98d50334d2cc86a9bc2bb5c7271e59c
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2025-07-09 17:39:25 +0800

    test deployment2

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.node_env === 'development' 
+  baseURL: process.env.NODE_ENV === 'development' 
     ? "http://localhost:5000/api" 
-    : "https://kardetailing2.onrender.com/api",
+    : "https://kar-detailing-services.onrender.com/api",
   withCredentials: true,
 });
 
